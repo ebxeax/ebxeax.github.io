3c8457d85683520dcbaf54ba47e75ce3a6e0fdcf
ebxeax <798193559@qq.com>
2023-01-09 17:53:09 +0800

    f

---
+++ toolbox/initUbuntu/initUbuntu.sh
@@ -0,0 +1,9 @@
+sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip git cmake 
+python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+
+
+pip3 install d2l
