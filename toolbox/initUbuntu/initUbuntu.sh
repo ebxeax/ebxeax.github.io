437f62d7cb36e569717d23ce45edf2f53eef6e42
ebxeax <798193559@qq.com>
2023-01-10 18:06:45 +0800

    f

--- toolbox/initUbuntu/initUbuntu.sh
+++ toolbox/initUbuntu/initUbuntu.sh
@@ -2,8 +2,8 @@ sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@
 sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade
-sudo apt install python3-pip git cmake 
+sudo apt install python3-pip git cmake nodejs npm 
 python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
 
 
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple d2l 
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple d2l jupyterlab
